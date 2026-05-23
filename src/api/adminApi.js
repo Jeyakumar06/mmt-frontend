27810e07a5633bd0ed5dc25a2000ad868868b7d4
Jeyakumar06 <jkjeyakumar56@gmail.com>
2026-05-23 09:01:57 +0530

    login  api path

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,5 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_BASE   = `${BASE_URL}/api/admin`;
+export const API_BASE   = `${BASE_URL}/admin`;
 export const IMAGE_BASE = BASE_URL;
 
 function getToken() {
--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const loginApi = (email, password) => {
   console.log("Attempting login with:", { email });
 
-  return api.post(`${BASE_URL}/api/admin/login`, {
+  return api.post(`${BASE_URL}/admin/login`, {
     email,
     password,
   });
